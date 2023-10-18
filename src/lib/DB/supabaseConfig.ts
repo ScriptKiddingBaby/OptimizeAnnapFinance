557ea5bd93809010865c9b0d58a823485514c0d3
ScriptKiddingBaby <scriptkidding1@gmail.com>
2023-10-18 17:51:32 +0800

    damn vite

--- src/lib/DB/supabaseConfig.ts
+++ src/lib/DB/supabaseConfig.ts
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 import { PUBLIC_VITE_SUPABASE_ANON_KEY, PUBLIC_VITE_SUPABASE_URL } from "$env/static/public";
 
-export const supabase = createClient(PUBLIC_VITE_SUPABASE_URL, PUBLIC_VITE_SUPABASE_ANON_KEY);
+export const supabase = createClient("https://aritmjndldlhjuevgtiv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyaXRtam5kbGRsaGp1ZXZndGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcwMDc3MzIsImV4cCI6MjAxMjU4MzczMn0.ZLESyTS2DFbDUnHWvMS-c1SrZuA4vkg9tq-diH0JK84");
