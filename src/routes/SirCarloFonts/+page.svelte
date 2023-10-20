4a0fa777f4927819f3d29bf9764797abceca9886
ScriptKiddingBaby <scriptkidding1@gmail.com>
2023-10-20 10:27:36 +0800

    WEWEWEW

---
+++ src/routes/SirCarloFonts/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+
+</script>
+
+<main class="">
+    <div class="mt-10 text-center">
+        <pre class="text-red-500 bg-black">Normal no bold</pre>
+        <p class="font-sans">Hello subscribe welcome blog to my "paragraph - sans"</p>
+        <p class="font-serif">Hello subscribe welcome blog to my "paragraph - serif"</p>
+        <p class="font-mono">Hello subscribe welcome blog to my "paragraph - mono"</p>
+    </div>
+
+    <div class="mt-10 text-center">
+        <pre>Normal has semi bold</pre>
+        <p class="font-sans font-semibold">Hello subscribe welcome blog to my "paragraph - sans"</p>
+        <p class="font-serif font-semibold">Hello subscribe welcome blog to my "paragraph - serif"</p>
+        <p class="font-mono font-semibold">Hello subscribe welcome blog to my "paragraph - mono"</p>
+    </div>
+
+    <div class="mt-10 text-center">
+        <pre class="text-red-500 bg-black">Normal no bold</pre>
+        <h1 class="font-sans h1 mt-5">welcome blog to my "heading v1 - sans"</h1>
+        <h1 class="font-sans h2">welcome blog to my "heading v2 - sans"</h1>
+        <h1 class="font-sans h3">welcome blog to my "heading v3 - sans"</h1>
+        <h1 class="font-sans h4">welcome blog to my "heading v4 - sans"</h1>
+        <h1 class="font-sans h5">welcome blog to my "heading v5 - sans"</h1>
+        <h1 class="font-sans h6">welcome blog to my "heading v6 - sans"</h1>
+
+        <h1 class="font-serif h1 mt-5">welcome blog to my "heading v1 - serif"</h1>
+        <h1 class="font-serif h2">welcome blog to my "heading v2 - serif"</h1>
+        <h1 class="font-serif h3">welcome blog to my "heading v3 - serif"</h1>
+        <h1 class="font-serif h4">welcome blog to my "heading v4 - serif"</h1>
+        <h1 class="font-serif h5">welcome blog to my "heading v5 - serif"</h1>
+        <h1 class="font-serif h6">welcome blog to my "heading v6 - serif"</h1>
+
+        <h1 class="font-mono h1 mt-5">welcome blog to my "heading v1 - mono"</h1>
+        <h1 class="font-mono h2">welcome blog to my "heading v2 - mono"</h1>
+        <h1 class="font-mono h3">welcome blog to my "heading v3 - mono"</h1>
+        <h1 class="font-mono h4">welcome blog to my "heading v4 - mono"</h1>
+        <h1 class="font-mono h5">welcome blog to my "heading v5 - mono"</h1>
+        <h1 class="font-mono h6">welcome blog to my "heading v6 - mono"</h1>
+    </div>
+
+    <div class="mt-10 text-center">
+        <pre class="text-red-500 bg-black">Normal semi bold</pre>
+        <h1 class="font-sans font-semibold h1 mt-5">welcome blog to my "heading v1 - sans"</h1>
+        <h1 class="font-sans font-semibold h2">welcome blog to my "heading v2 - sans"</h1>
+        <h1 class="font-sans font-semibold h3">welcome blog to my "heading v3 - sans"</h1>
+        <h1 class="font-sans font-semibold h4">welcome blog to my "heading v4 - sans"</h1>
+        <h1 class="font-sans font-semibold h5">welcome blog to my "heading v5 - sans"</h1>
+        <h1 class="font-sans font-semibold h6">welcome blog to my "heading v6 - sans"</h1>
+
+        <h1 class="font-serif font-semibold h1 mt-5">welcome blog to my "heading v1 - serif"</h1>
+        <h1 class="font-serif font-semibold h2">welcome blog to my "heading v2 - serif"</h1>
+        <h1 class="font-serif font-semibold h3">welcome blog to my "heading v3 - serif"</h1>
+        <h1 class="font-serif font-semibold h4">welcome blog to my "heading v4 - serif"</h1>
+        <h1 class="font-serif font-semibold h5">welcome blog to my "heading v5 - serif"</h1>
+        <h1 class="font-serif font-semibold h6">welcome blog to my "heading v6 - serif"</h1>
+
+        <h1 class="font-mono font-semibold h1 mt-5">welcome blog to my "heading v1 - mono"</h1>
+        <h1 class="font-mono font-semibold h2">welcome blog to my "heading v2 - mono"</h1>
+        <h1 class="font-mono font-semibold h3">welcome blog to my "heading v3 - mono"</h1>
+        <h1 class="font-mono font-semibold h4">welcome blog to my "heading v4 - mono"</h1>
+        <h1 class="font-mono font-semibold h5">welcome blog to my "heading v5 - mono"</h1>
+        <h1 class="font-mono font-semibold h6">welcome blog to my "heading v6 - mono"</h1>
+    </div>
+
+
+</main>
